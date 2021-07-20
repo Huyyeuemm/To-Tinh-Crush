fbfa97cdb59da4529ef51447d3daea2d0f608d02
Huyyeuemm <87706508+Huyyeuemm@users.noreply.github.com>
2021-07-20 21:24:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'Bé ơii',
+    introDesc: `Tớ hỏi bé 1 câu bé phại trả lời thật lòng nhóaaa`,
+    btnIntro: 'Ấn đấy tiếp đi nàiii',
+    title: 'Hi bé yêu tớ phải hemmm',
+    desc: 'Chọn bên dưới đơi nàii ',
+    btnYes: 'Cóa chớ hii',
+    btnNo: 'Không',
+    question:'Ỏ b chọn cóa hãa',
+    btnReply: 'Type sth xong gửi choa tuiii',
+    reply: 'Yêu thì yêu mà không yêu thì yêu ',
+    mess: 'Tớ cũng yêu bé nhiều nắm 😘',
+    messDesc: 'Mai mừn đi chơi nháaa.',
+    btnAccept: 'Okiiiii lun ',
+    messLink: 'https://www.facebook.com/profile.php?id=100023062697599
 }
